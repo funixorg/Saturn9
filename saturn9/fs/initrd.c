14996829c7640306bebb9edea25edf084aa4e280
unitythecoder <unitythecodergit@gmail.com>
2023-11-26 15:34:58 +0100

    [+Feat] { Small initrd fix }

--- saturn9/fs/initrd.c
+++ saturn9/fs/initrd.c
@@ -73,7 +73,7 @@ fs_node_t *initialise_initrd(uint32_t location)
 	initrd_root->impl	= 0;
 
 	// Initialise the /dev directory (required!)
-	initrd_dev = (fs_node_t*)kmalloc(sizeof(fs_node_t));
+	initrd_dev = (fs_node_t*)memalloc(sizeof(fs_node_t));
 	strcpy(initrd_dev->name, "dev");
 	initrd_dev->mask	= initrd_dev->uid = initrd_dev->gid = initrd_dev->inode = initrd_dev->length = 0;
 	initrd_dev->flags	= FS_DIRECTORY;
@@ -86,7 +86,7 @@ fs_node_t *initialise_initrd(uint32_t location)
 	initrd_dev->ptr		= 0;
 	initrd_dev->impl	= 0;
 
-	root_nodes	= (fs_node_t*)kmalloc(sizeof(fs_node_t) * initrd_header->nfiles);
+	root_nodes	= (fs_node_t*)memalloc(sizeof(fs_node_t) * initrd_header->nfiles);
 	nroot_nodes	= initrd_header->nfiles;
 
 	// For every file...
